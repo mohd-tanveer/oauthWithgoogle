9489f751b5687ddabbbac946e097a8d32482c6c2
mohd-tanveer <tanveer18596@gmail.com>
2022-04-11 22:11:39 +0530

    implementing login with facebook and github from scratch

---
+++ src/main/java/com/learning/oauthWithgoogle/OauthWithgoogleApplication.java
@@ -0,0 +1,13 @@
+package com.learning.oauthWithgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthWithgoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthWithgoogleApplication.class, args);
+	}
+
+}
